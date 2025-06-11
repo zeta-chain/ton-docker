c41af6d9eac075af8df3a465f1bc705b00a4367a
Dmitry S <11892559+swift1337@users.noreply.github.com>
2025-06-11 15:02:14 +0200

    Include faucet in the healthcheck (#9)

--- sidecar/main.go
+++ sidecar/main.go
@@ -9,6 +9,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/url"
 	"os"
 	"os/exec"
 	"time"
@@ -109,6 +110,10 @@ func statusHandler(w http.ResponseWriter, _ *http.Request) error {
 		return fmt.Errorf("rpc check failed: %w", err)
 	}
 
+	if err := checkFaucet(ctx); err != nil {
+		return fmt.Errorf("faucet check failed: %w", err)
+	}
+
 	jsonResponse(w, http.StatusOK, resOK)
 
 	return nil
@@ -176,25 +181,8 @@ func checkLiteClient(ctx context.Context) error {
 
 // https://toncenter.com/api/v2/#/
 func checkRPC(ctx context.Context) error {
-	url := fmt.Sprintf("http://0.0.0.0:%s/getMasterchainInfo", rpcPort)
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
-	if err != nil {
-		return fmt.Errorf("failed to create request: %w", err)
-	}
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("failed to get masterchain info: %w", err)
-	}
-
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("failed to get masterchain info: http code %d", resp.StatusCode)
-	}
-
-	type resShape struct {
+	// 1. Check masterchain info
+	type masterchainInfoResShape struct {
 		Ok     bool `json:"ok"`
 		Result struct {
 			Last struct {
@@ -203,20 +191,103 @@ func checkRPC(ctx context.Context) error {
 		} `json:"result"`
 	}
 
-	var res resShape
+	var masterchainInfoRes masterchainInfoResShape
 
-	err = json.NewDecoder(resp.Body).Decode(&res)
-
+	err := rpcRequestMapper(ctx, "getMasterchainInfo", nil, &masterchainInfoRes)
 	switch {
 	case err != nil:
-		return fmt.Errorf("failed to decode response: %w", err)
-	case !res.Ok:
-		return fmt.Errorf("invalid response")
-	case res.Result.Last.Seqno == 0:
+		return err
+	case !masterchainInfoRes.Ok:
+		return fmt.Errorf("invalid masterchain info response")
+	case masterchainInfoRes.Result.Last.Seqno == 0:
+		return fmt.Errorf("masterchain seqno is 0")
+	}
+
+	return nil
+}
+
+func checkFaucet(ctx context.Context) error {
+	type walletResShape struct {
+		Ok     bool `json:"ok"`
+		Result struct {
+			Wallet   bool   `json:"wallet"`
+			Seqno    uint32 `json:"seqno"`
+			WalletId int    `json:"wallet_id"`
+			Status   string `json:"account_state"`
+		} `json:"result"`
+	}
+
+	var faucetWallet walletResShape
+
+	query := map[string]string{"address": faucetBaseChain.WalletRawAddress}
+
+	err := rpcRequestMapper(ctx, "getWalletInformation", query, &faucetWallet)
+	switch {
+	case err != nil:
+		return err
+	case !faucetWallet.Ok:
+		return fmt.Errorf("invalid wallet response")
+	case faucetWallet.Result.Status != "active":
+		return fmt.Errorf("address is not active")
+	case !faucetWallet.Result.Wallet:
+		return fmt.Errorf("address is not a wallet")
+	case faucetWallet.Result.WalletId != faucetBaseChain.SubWalletId:
+		return fmt.Errorf("sub wallet id mismatch")
+	case faucetWallet.Result.Seqno == 0:
 		return fmt.Errorf("seqno is 0")
 	}
 
 	return nil
+}
+
+func rpcRequestMapper(ctx context.Context, method string, params map[string]string, tt any) error {
+	resp, err := rpcRequest(ctx, method, params)
+	if err != nil {
+		return fmt.Errorf("failed to create request: %w", err)
+	}
+
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("failed to get %s: http code %d", method, resp.StatusCode)
+	}
+
+	err = json.NewDecoder(resp.Body).Decode(tt)
+	if err != nil {
+		return fmt.Errorf("%s: failed to decode response: %w", method, err)
+	}
+
+	return nil
+}
+
+func rpcRequest(ctx context.Context, method string, params map[string]string) (*http.Response, error) {
+	baseURL := fmt.Sprintf("http://0.0.0.0:%s/%s", rpcPort, method)
+
+	u, err := url.Parse(baseURL)
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse URL: %w", err)
+	}
+
+	// Add query parameters if provided
+	if len(params) > 0 {
+		q := u.Query()
+		for key, value := range params {
+			q.Add(key, value)
+		}
+		u.RawQuery = q.Encode()
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u.String(), nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, fmt.Errorf("failed to do request: %w", err)
+	}
+
+	return res, nil
 }
 
 func env(key, fallback string) string {
